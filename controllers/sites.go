38c1513930023910e0ec6918ba6e2b8e214316c1
Alexander Woods <alex.w@ware2go.co>
2020-05-18 16:43:31 -0400

    Add update and get

--- controllers/sites.go
+++ controllers/sites.go
@@ -11,6 +11,8 @@ func Routes(router *gin.Engine) {
 	sites := router.Group("/sites")
 	{
 		sites.POST("", CreateSite)
+		sites.POST("/:id", UpdateSite)
+		sites.GET("/:id", GetSite)
 	}
 }
 
@@ -19,4 +21,17 @@ func CreateSite(c *gin.Context) {
 	html := string(body)
 
 	c.String(200, internal.CreateSite(html))
+}
+
+func UpdateSite(c *gin.Context) {
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	html := string(body)
+
+	c.String(200, internal.CreateSite(html))
+}
+
+func GetSite(c *gin.Context) {
+	id := c.Param("id")
+
+	c.String(200, "getting site "+id)
 }
