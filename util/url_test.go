d8ef685d1934745dfe50404f467f5e44c3b397be
Alexander Woods <alex.w@ware2go.co>
2020-05-21 17:39:04 -0400

    Turn Canary test into url test

--- util/url_test.go
+++ util/url_test.go
@@ -2,10 +2,24 @@ package util
 
 import "testing"
 
-func TestCanary(t *testing.T) {
-	x := 4
+func TestInvertUrl(t *testing.T) {
+	urls := []string{
+		"https://www.atomiccommits.io/joining-streams/",
+		"http://www.atomiccommits.io/inline-functions/",
+		"https://vim-adventures.com",
+		"https://mail.google.com/mail/u/0/#inbox",
+	}
 
-	if x == 4 {
-		t.Errorf("x should not equal 4, it is")
+	invertedUrls := []string{
+		"io.atomiccommits.www/joining-streams/",
+		"io.atomiccommits.www/inline-functions/",
+		"com.vim-adventures",
+		"com.google.mail/mail/u/0/#inbox",
+	}
+
+	for i, url := range urls {
+		if InvertUrl(url) != invertedUrls[i] {
+			t.Errorf("Url " + url + " should invert to " + invertedUrls[i] + ", but it inverts to " + InvertUrl(url))
+		}
 	}
 }
